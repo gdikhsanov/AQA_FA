3f6ebeff9d39e10788f5f352448bb2dd0541c985
gdikhsanov <gdikhsanov@gmail.com>
2022-09-26 12:17:53 +0300

    mac HW_4 rename to 3

--- src/HW/HW3.java
+++ src/HW/HW3.java
@@ -1,122 +1,190 @@
 package HW;
 
-public class HW3 {
+import java.util.Arrays;
+import java.util.Objects;
 
+public class HW3 {
     public static void main(String[] args) {
 
-        int task = 0;
-        System.out.println("-----------------\nЗадача " + ++task + "\n-----------------");
+        int number = 0;
 
-        int a = 33;
-        int b = 15;
+        //      #1
 
-        if (a == b) {
-            System.out.println(a + " == " + b);
-        } else if (a < b) {
-            System.out.println(a + " < " + b);
-        } else {
-            System.out.println(a + " > " + b);
+        System.out.println("\n############################## \n"
+                + "Task "
+                + ++number);
+
+        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+        int s = 0;
+
+        for (int i = 0; i < array.length; i++) {
+
+            s += array[i];
+
+        }
+        System.out.println(s);
+
+        //      #2
+
+        System.out.println("\n############################## \n"
+                + "Task "
+                + ++number);
+
+        //System.out.println(Arrays.stream(array).max());
+
+        s = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+
+            if (array[i] > s)
+                s = array[i];
         }
 
-        System.out.println("-----------------\nЗадача " + ++task + "\n-----------------");
+        System.out.println(s);
 
-        int a1 = 33;
-        int b1 = 15;
+        //      #3
 
-        if ((a1 + b1) % 2 == 0) {
-            System.out.printf("maybe %d and %d are even\n", a1, b1);
-        } else {
-            System.out.println("some varible is odd");
+        System.out.println("\n############################## \n"
+                + "Task "
+                + ++number);
+
+        s = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+
+            if (array[i] < s)
+                s = array[i];
         }
 
-        System.out.println("-----------------\nЗадача " + ++task + "\n-----------------");
+        System.out.println(s);
 
-        int a3 = 300;
+        //      #4
 
-        if (a3 > 10) {
-            System.out.println(a3 + " больше 10");
-        }
-        if (a3 < 100) {
-            System.out.println(a3 + " меньше 100");
-        }
-        if (a3 / 2 > 20) {
-            System.out.println("результат деления " + a3 + " на 2 больше 20");
-        }
-        if (5 <= a3 && a3 <= 40) {
-            System.out.println("значение переменной " + a3 + "  между 5 и 40 включительно");
-        } else {
-            System.out.println("значение переменной " + a3 + "  меньше 5 или больше 40");
+        System.out.println("\n############################## \n"
+                + "Task "
+                + ++number);
+
+        int[] array1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        s = 0;
+
+        for (int i = 0; i < array1.length; i++) {
+
+            s += array1[i];
         }
 
-        System.out.println("-----------------\nЗадача " + ++task + "\n-----------------");
+        System.out.println(s / array1.length);
 
-        for (int i = 0; i <= 15; i++) {
-            System.out.println(i);
-        }
+        //      #5
 
-        System.out.println("-----------------\nЗадача " + ++task + "\n-----------------");
+        System.out.println("\n############################## \n"
+                + "Task "
+                + ++number);
 
-        int a4 = 5;
-        int result = 10000;
+        s =0;
+        int[][] array2 = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
 
-        System.out.printf("Положительные степени числа %d, которые меньше %d.\n", a4, result);
+        for (int i = 0; i < array2.length; i++) {
 
-        for (int i = result; i > 1; i--) {
+            for (int j = 0; j < array2[i].length; j++) {
 
-            if (Math.log(i) % Math.log(a4) < 0.00001){     // дробная часть логарифма i с основанием a4
-                System.out.printf("%d в степени %d = %d\n", a4, (int)(Math.log(i) / Math.log(a4)), i);
-            }
-        }
-/**
- *     System.out.println((int)Math.pow(5,3)); // 3^4
- *     Math.log(i) / Math.log(a4) //логарифм i с основанием a4
- *     System.out.println((Math.log(125) / Math.log(5))); // выдаёт 3.0000000000000004 Надо переводить в decimal???
- */
+                s += array2[i][j];
 
-
-        System.out.println("-----------------\nЗадача " + task + " с возведением в степень\n-----------------");
-
-        int a5 = 5;
-        int result1 = 10000;
-        double maxSt = (Math.log(result1) / Math.log(a5));
-
-        System.out.printf("Положительные степени числа %d, которые меньше %d.\n", a4, result1);
-
-        for (int i = 1; i <= maxSt; i++) {
-
-            if (Math.pow(5,i) <= result1){
-                System.out.printf("%d в степени %d = %d\n", a5, i, (int)(Math.pow(5,i)));
-            }
-        }
-
-        System.out.println("-----------------\nЗадача " + ++task + " a)\n-----------------");
-
-        int from = 40;
-        int to = 60;
-        int a6 = 4;
-
-        System.out.printf("Числа кратные %d между числами %d и %d включительно.\n", a6, from, to);
-
-        for (int i = from; i <= to; i++) {
-
-            if ((i % a6 == 0)) {
-                System.out.println(i);
             }
         }
 
-        System.out.println("-----------------\nЗадача " + task + " b)\n-----------------");
+        System.out.println(s);
 
-        from = 40;
-        to = 60;
-        a6 = 4;
+        //      #6
 
-        System.out.printf("Числа кратные %d между числами %d и %d включительно.\n", a6, from, to);
+        System.out.println("\n############################## \n"
+                + "Task "
+                + ++number);
 
-        for (int i = from; i <= to; i=i+4) {
+        s =0;
 
-            System.out.println(i);
+        for (int i = 0; i < array2.length; i++) {
+
+            for (int j = 0; j < array2[i].length; j++) {
+
+                if (array2[i][j] > s) {
+                    s = array2[i][j];
+                }
+            }
+        }
+
+        System.out.println(s);
+
+        //      #7
+
+        System.out.println("\n############################## \n"
+                + "Task "
+                + ++number);
+
+        s =0;
+
+        for (int i = 0; i < array2.length; i++) {
+
+            for (int j = 0; j < array2[i].length; j++) {
+
+                s++;
+
+            }
+        }
+
+        System.out.println(s);
+
+        //      #8 через массив
+
+        System.out.println("\n############################## \n"
+                + "Task "
+                + ++number);
+
+        int size = 20;                      //размер квадратного поля
+        String[] c = new String[size];      //Пустой массив
+        String x = "x";                     //служебный символ
+        String brush = "****";                 //кисть - чем рисуем - любая строка, кроме "x". Хоть xxxxxxxxxx
+        String width = "   ";               // Количеством пробелов выбираем ширину круга
+
+        for (int i = size; i >= 0; i--) {   // i - это координата Y
+
+                                            //общая формула (x – a)2 + (y – b)2 = R2, где а и b - это координаты центра
+
+            int xCoordR = (int)             // это формула координаты Х правой части
+                Math.round(Math.sqrt(Math.pow((size) / 2.0, 2) - Math.pow((i) - size / 2.0, 2)) + size/2.0 - 1);
+
+            int xCoordL = (int)             // это формула координаты Х левой части
+                (size/2.0 - Math.round(Math.sqrt(Math.pow((size) / 2.0, 2) - Math.pow((i) - size / 2.0, 2))));
+
+            c[xCoordR] = x;                 //присваиваем правый символ ячейке массива
+            c[xCoordL] = x;                 //присваиваем левый символ ячейке массива
+
+             for (int j = 0; j < size; j++) {      // стираем лишние символы из массивов прошлых строк
+                if (Objects.equals(c[j], x)) {     // т.к. они накапливаются в массиве
+
+                    c[j] = brush;               // рисуем символ кисти в занятые ячейку
+                }
+                else c[j] = width;              //в пустые рисуем пробелы
+             }
+
+           System.out.println(Arrays.toString(c).replace(", ","")); //печатаем строки, удаляя запятые.
+         }
+
+        //      #8.1 без массивов
+
+        //размер квадратного поля
+
+        String brush1 = "*";                 //кисть - чем рисуем - любая строка, кроме "x". Хоть xxxxxxxxxx
+        String spases = "   ";               // Количеством пробелов выбираем ширину круга
+
+        for (int i = size; i >= 0; i--) {
+
+            //общая формула (x – a)2 + (y – b)2 = R2, где а и b - это координаты центра
+
+            int xx = (int)
+                    (size/2.0 - Math.round(Math.sqrt(Math.pow((size) / 2.0, 2) - Math.pow((i) - size / 2.0, 2))));
+
+            System.out.println(spases.repeat(xx) + brush1 + spases.repeat(size-xx-xx) + brush1); //печатаем строки.
         }
     }
 }
-
-
