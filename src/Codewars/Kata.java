d9399a2484574cafb191984282e75fe5d74751ac
gdikhsanov <gdikhsanov@gmail.com>
2022-10-09 16:43:06 +0300

    mac HW_6 all done

--- src/Codewars/Kata.java
+++ src/Codewars/Kata.java
@@ -1,19 +1,73 @@
 package Codewars;
 
-class Kata {
-    public static String countingSheep(int num) {
-        //Add your code here
-        StringBuilder str = new StringBuilder();
+public class Kata {
 
-        for (int i = 1; i <= num; i++) {
-            str.append(i).append(" sheep...");
+    public static String declareWinner(Fighter fighter1, Fighter fighter2, String firstAttacker) {
+
+        int f1 = fighter1.health;
+        int f2 = fighter2.health;
+        String winner = null;
+
+        if (firstAttacker.equals(fighter1.name)) {
+
+            while (f1 > 0 || f2 > 0) {
+
+                f2 -= fighter1.damagePerAttack;
+                System.out.print(fighter1.name + " attacks " + fighter2.name + "; " + fighter2.name + " now has " + f2 + " health");
+                if (f2 <= 0) {
+                    System.out.println(" and is dead. " + fighter1.name + " wins.");
+                    winner = fighter1.name;
+                    break;
+                }
+                else {
+                    System.out.println(".");
+                }
+
+                f1 -= fighter2.damagePerAttack;
+                System.out.print(fighter2.name + " attacks " + fighter1.name + "; " + fighter1.name + " now has " + f1 + " health");
+                if (f1 <= 0) {
+                    System.out.println(" and is dead. " + fighter2.name + " wins.");
+                    winner = fighter2.name;
+                    break;
+                }
+                else {
+                    System.out.println(".");
+                }
+            }
+
+            return winner;
         }
+        else {
+            while (f1 > 0 || f2 > 0) {
 
-        return str.toString();
+                f1 -= fighter2.damagePerAttack;
+                System.out.print(fighter2.name + " attacks " + fighter1.name + "; " + fighter1.name + " now has " + f1 + " health");
+                if (f1 <= 0) {
+                    System.out.println(" and is dead. " + fighter2.name + " wins.");
+                    winner = fighter2.name;
+                    break;
+                }
+                else {
+                    System.out.println(".");
+                }
+
+                f2 -= fighter1.damagePerAttack;
+                System.out.print(fighter1.name + " attacks " + fighter2.name + "; " + fighter2.name + " now has " + f2 + " health");
+                if (f2 <= 0) {
+                    System.out.println(" and is dead. " + fighter1.name + " wins.");
+                    winner = fighter1.name;
+                    break;
+                }
+                else {
+                    System.out.println(".");
+                }
+            }
+            return winner;
+        }
     }
 
     public static void main(String[] args) {
-        System.out.print(countingSheep(3));
-
+        declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew");
     }
+
 }
--- src/Codewars/SolutionTest.java
+++ src/Codewars/SolutionTest.java
@@ -2,14 +2,13 @@ package Codewars;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import org.junit.runners.JUnit4;
 
 public class SolutionTest {
     @Test
     public void testSomething() {
-        assertEquals("", Kata.countingSheep(0));
-        assertEquals("1 sheep...", Kata.countingSheep(1));
-        assertEquals("1 sheep...2 sheep...", Kata.countingSheep(2));
-        assertEquals("1 sheep...2 sheep...3 sheep...", Kata.countingSheep(3));
+        assertEquals("", Kata0.countingSheep(0));
+        assertEquals("1 sheep...", Kata0.countingSheep(1));
+        assertEquals("1 sheep...2 sheep...", Kata0.countingSheep(2));
+        assertEquals("1 sheep...2 sheep...3 sheep...", Kata0.countingSheep(3));
     }
 }
